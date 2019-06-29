34302143197217ec6ce16756137ea185e2deaee5
kungfu <1378647642@qq.com>
2019-06-29 20:40:16 +0800

    YFK

---
+++ Assets/Scripts/BuffAdditionAutoCreate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffAdditionAutoCreate : MonoBehaviour
+{
+    [Header("Buff种类")]
+    public GameObject BuffAddition_1;
+    public GameObject BuffAddition_2;
+    public GameObject BuffAddition_3;
+    public GameObject BuffAddition_4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ContinueShakeCamera_1.cs
+++ Assets/Scripts/ContinueShakeCamera_1.cs
@@ -4,47 +4,47 @@ using UnityEngine;
 
 public class ContinueShakeCamera_1 : MonoBehaviour
 {
-    [Tooltip("振动时间")]
-    public static float ShakeTime = 1f;
-    private static float shakeTime;
+    //[Tooltip("振动时间")]
+    //public static float ShakeTime = 1f;
+    //private static float shakeTime;
 
-    private float shakeDelta = 0.005f;
-    public Camera cam;
-    private static bool isShakeCamera = false;
-    // Use this for initialization
-    void Start()
-    {
-        shakeTime = ShakeTime;
+    //private float shakeDelta = 0.005f;
+    //public Camera cam;
+    //private static bool isShakeCamera = false;
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    shakeTime = ShakeTime;
 
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isShakeCamera)
-        {
-            if (shakeTime > 0)
-            {
-                shakeTime -= Time.deltaTime;
-                if (shakeTime <= 0)
-                {
-                    cam.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
-                    isShakeCamera = false;
-                    shakeTime = ShakeTime;
-                }
-                else
-                {
-                        cam.rect = new Rect(shakeDelta * (-1.0f + 2.0f * Random.value), shakeDelta * (-1.0f + 2.0f * Random.value), 1.0f, 1.0f);
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (isShakeCamera)
+    //    {
+    //        if (shakeTime > 0)
+    //        {
+    //            shakeTime -= Time.deltaTime;
+    //            if (shakeTime <= 0)
+    //            {
+    //                cam.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+    //                isShakeCamera = false;
+    //                shakeTime = ShakeTime;
+    //            }
+    //            else
+    //            {
+    //                    cam.rect = new Rect(shakeDelta * (-1.0f + 2.0f * Random.value), shakeDelta * (-1.0f + 2.0f * Random.value), 1.0f, 1.0f);
 
-                }
-            }
-        }
+    //            }
+    //        }
+    //    }
 
 
-    }
-    public static void panduan() {
-        isShakeCamera = true;
-        shakeTime = ShakeTime;
-    }
+    //}
+    //public static void panduan() {
+    //    isShakeCamera = true;
+    //    shakeTime = ShakeTime;
+    //}
 
 }
